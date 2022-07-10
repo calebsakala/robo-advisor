ae99efb2c0f64e5bc5f428635d60c77a8154c893
“calebsakala” <admin@calebsakala.com>
2022-07-10 21:34:58 +0300

    1. Changed the variable names to make them easier to understand. 2. Changed the layout of the code to make it more re-usable. (Changing the converter into a function that will take any file's path and output the date as a number).

--- stock_date_converter.py
+++ stock_date_converter.py
@@ -16,32 +16,35 @@ def date_in_days(year, month, day):
 	return delta.days
 
 
-file_to_edit = input("What stock file would you like to edit? > ")
-new_file_name = input("\nWhat would you like to rename the new file we will create? > ")
+"""Defining a function to recreate the file with the new dates"""
+
+def recreate_file(old_file_name, new_file_name, convert=date_in_days):
+	"""Both files are opened"""
+
+	with open(new_file_name, "w") as newfile:
+		with open(old_file_name, "r") as oldfile:
+
+			"""The first line is written to the new file immediately as it is always 
+			just a header"""
+			newfile.write(oldfile.readline())
+
+			"""Then the date is extracted from every other file"""
+			for line in oldfile.readlines():
+				date = line.split(',')[0]
+				rest_of_line = line.split(',')[1:]
+				date_list = date.split('-')
+				years = int(date_list[0])
+				months = int(date_list[1])
+				days = int(date_list[2])
+
+				"""The date is converted to days"""
+				date_distance = convert(years, months, days)
+
+				"""And the output is joined back with the rest of the lines in the new file"""
+				newfile.write(str(date_distance) + "," + ",".join(rest_of_line))
 
 
-"""Both files are opened"""
+read_file = input("What stock file would you like to edit? > ")
+write_file = input("\nWhat would you like to rename the new file we will create? > ")
 
-with open(new_file_name, "w") as newfile:
-	with open(file_to_edit, "r") as oldfile:
-
-		"""The first line is written to the new file immediately as it is always 
-		just a header"""
-		newfile.write(oldfile.readline())
-
-		"""Then the date is extracted from every other file"""
-		for line in oldfile.readlines():
-			date = line.split(',')[0]
-			rest_of_line = line.split(',')[1:]
-			date_list = date.split('-')
-			years = int(date_list[0])
-			months = int(date_list[1])
-			days = int(date_list[2])
-
-			"""The date is converted to days"""
-			date_distance = date_in_days(years, months, days)
-
-			"""And the output is joined back with the rest of the lines in the new file"""
-			newfile.write(str(date_distance) + "," + ",".join(rest_of_line))
-
-
+recreate_file(read_file, write_file)
